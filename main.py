48a1fafa7ff6fe0292e4b8642bc94dfe255da88c
malluhackr <cristi7jjr@gmail.com>
2025-08-13 13:51:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,31 +29,8 @@ from pyrogram.types import (
     ReplyKeyboardRemove
 )
 
-# Instagram Client
-from instagrapi import Client as InstaClient
-from instagrapi.exceptions import (
-    LoginRequired,
-    ChallengeRequired,
-    BadPassword,
-    PleaseWaitFewMinutes,
-    ClientError
-)
-from instagrapi.types import Usertag, Location, StoryMention, StoryLocation, StoryHashtag, StoryLink
-
 # Twitter Client
 from twscrape import API, AccountsPool
-
-# This block correctly handles exception imports for different versions of twscrape
-try:
-    # Path for recent versions like 0.17.0
-    from twscrape.accounts_pool import HTTPException as XAPIException
-except ModuleNotFoundError:
-    try:
-        # Path for slightly older versions (>=0.12.0)
-        from twscrape.exceptions import HTTPException as XAPIException
-    except ModuleNotFoundError:
-        # Path for very old versions (<0.12.0)
-        from twscrape.http import HTTPException as XAPIException
 
 # System Utilities
 import psutil
